4f7be6f01a9d57d12bdb192cbfdb3647ac4e8eaa
Jonas <leipuri.junnuinen@gmail.com>
2021-04-24 16:49:36 +0300

    pzip big upgrades, ready for punzip

--- Projects/project_5_parallel_zip/pzip.c
+++ Projects/project_5_parallel_zip/pzip.c
@@ -3,59 +3,98 @@
 #include <string.h>
 #include <sys/sysinfo.h>
 #include <pthread.h>
+#include <time.h>
 
 
-// Linked list node struct
-typedef struct node {
+
+// ======== STRUCTS ========
+// Linked list node structure
+typedef struct ll_node {
+    // Data can be anything
+    void * data; 
+    struct ll_node * next;
+} ll_node_t;
+
+
+// Datatype for num char pairs created by compression
+// ... 10A, B36, K3, Y12, ...
+typedef struct num_char {
     char character;
     int count;
-    struct node * next;
-} node_t;
+} num_char_t;
 
+
+// Datatype threads access to get next task
+// filename = what file the need to access and process
+// index at what index in head_list should the created num_char list go
+typedef struct th_data_struct {
+    char * filename;
+    int    index;
+} th_data;
+
+
+
+
+// ======== GLOBALS FOR THREADS ========
+// List of list heads
+ll_node_t ** head_list = NULL;
+// Global head_list_lock
+pthread_mutex_t head_list_lock;
+
+// Number that tells how many threads are busy
+int busy_th_count = 0;
+// Global lock for busy_threads
+pthread_mutex_t busy_th_count_lock;
+
+
+
+
+
+// ======== LINKED LIST FUNCTIONS ========
 // To init list and return head
-node_t * initLinkedList(char c, int cnt) {
+ll_node_t * initList(void * data) {
 
-    node_t * head;
-    if ((head = (node_t *) calloc(1, sizeof(node_t))) == NULL) {
+    ll_node_t * head;
+    if ((head = (ll_node_t *) calloc(1, sizeof(ll_node_t))) == NULL) {
         perror("init linked");
         exit(-1);
     }
-    head->character = c;
-    head->count     = cnt;
+    head->data      = data;
     head->next      = NULL;
 
     return head;
 }
 
 // Function to add node to the end of list
-int addToLinkedList(node_t * head, char c, int cnt) {
-    node_t * current = head;
+int addToEndOfList(ll_node_t * head, void * data) {
+    ll_node_t * current = head;
     while (current->next != NULL) {
         current = current->next;
     }
 
     // Variables
-    if ((current->next = (node_t *) calloc(1, sizeof(node_t))) == NULL) {
+    if ((current->next = (ll_node_t *) calloc(1, sizeof(ll_node_t))) == NULL) {
         perror("add to linked");
         exit(-1);
     }
-    current->next->character    = c;
-    current->next->count        = cnt;
+    current->next->data         = data;
     current->next->next         = NULL;
     return 0;
 }
 
 // Function to traverse and free the list
-int freeLinkedList(node_t * head) {
+int freeLinkedList(ll_node_t * head) {
     if (head != NULL) {
 
-        node_t * temp1 = head;
-        node_t * temp2 = NULL;
+        ll_node_t * temp1 = head;
+        ll_node_t * temp2 = NULL;
 
         while ((temp2 = temp1->next) != NULL) {
+            free(temp1->data);
             free(temp1);
             temp1 = temp2;
         }
+        free(temp1->data);
         free(temp1);
         free(temp2);
     }
@@ -63,10 +102,10 @@ int freeLinkedList(node_t * head) {
 }
 
 // Function to return pointer to the tail of given list
-node_t * getTailLinkedList(node_t * head) {
+ll_node_t * getTailLinkedList(ll_node_t * head) {
     if (head != NULL) {
 
-        node_t * curr = head;
+        ll_node_t * curr = head;
         // Traverse to the end of list
         while (curr->next != NULL)
         {
@@ -77,26 +116,23 @@ node_t * getTailLinkedList(node_t * head) {
     return NULL;
 }
 
+// Shift function removes and returns the head of the list
+// while updating the head to be the next
+ll_node_t * popLinkedList(ll_node_t ** head) {
+    if (* head != NULL) {
+        ll_node_t * first =  * head;
+        ll_node_t * next  = (* head)->next;
+        * head = next; // replacing head with next
+        return first;
+    }
+    return NULL;
+}
 
 
 
 
 
-// GLOBALS
-// List of list heads
-node_t ** head_list = NULL;
-// Global head_list_lock
-pthread_mutex_t head_list_lock;
 
-
-
-
-
-// Data strcut for threadFunction
-typedef struct th_data_struct {
-    char * filename;
-    int    index;
-} th_data;
 
 
 void * threadFunction(void * in_data) {
@@ -113,7 +149,13 @@ void * threadFunction(void * in_data) {
     ssize_t read;
 
 
-    node_t * head = NULL;
+    ll_node_t * head = NULL;
+
+    // get lock before using global int
+    pthread_mutex_lock(&busy_th_count_lock);
+    // add one being busy (this thread)
+    busy_th_count++;
+    pthread_mutex_unlock(&busy_th_count_lock);
 
     if ((file = fopen(filename, "r")) == NULL) {
         fprintf(stderr, "error: cannot open file '%s'\n", filename);
@@ -148,11 +190,22 @@ void * threadFunction(void * in_data) {
             }
             
             else {
+
+                num_char_t * num_char_pair;
+                if ((num_char_pair = calloc(1, sizeof(num_char_t))) == NULL) {
+                    perror("num_char_pair");
+                    exit(-1);
+                }
+
+                num_char_pair->character = prevChar;
+                num_char_pair->count     = count;
+
+
                 if (head != NULL)
                     last:
-                    addToLinkedList(head, prevChar, count);
+                    addToEndOfList(head, num_char_pair);
                 else
-                    head = initLinkedList(prevChar, count);
+                    head = initList(num_char_pair);
                 
                 count = 1;
             }
@@ -161,18 +214,24 @@ void * threadFunction(void * in_data) {
         }
     }
 
-    // Free allocations
-    fclose(file);
-    free(line);
-
     // get lock before using global list
     pthread_mutex_lock(&head_list_lock);
-
+    // add the head of this thread to head_list
     head_list[file_num] = head;
-    // release the lock after adding list head
     pthread_mutex_unlock(&head_list_lock);
 
+
+    // get lock before using global int
+    pthread_mutex_lock(&busy_th_count_lock);
+    // remove one busy thread (this)
+    busy_th_count--;
+    pthread_mutex_unlock(&busy_th_count_lock);
+
+
+
     // free and detach all thread memory
+    fclose(file);
+    free(line);
     free(data);
     pthread_detach(pthread_self());
     
@@ -185,74 +244,137 @@ void * threadFunction(void * in_data) {
 
 int main(int argc, char * argv[]) {
 
-    node_t * main_head = NULL;
+    ll_node_t * main_head = NULL;
 
     // Number of system available threads
-    //int th_count = get_nprocs();
+    int th_count = 1;//get_nprocs();
 
     // Number of files to be read
-    int file_count = argc-2;
-    int th_count = file_count;
-
-    // List of pointers to thread_id pointers
-    pthread_t ** thread_list;
-    if ((thread_list = calloc(th_count, sizeof(pthread_t *))) == NULL) {
-        perror("thread_list");
-        exit(-1);
+    int file_count = argc-1;
+    
+    // No need to create more threads than there are files to assign them
+    if (file_count < th_count) {
+        th_count = file_count;
     }
 
-    // List of pointers to list heads
-    if ((head_list = calloc(th_count, sizeof(node_t *))) == NULL) {
-        perror("head_list");
-        exit(-1);
-    }
-
-    // Test if lock works
-    if (pthread_mutex_init(&head_list_lock, NULL) != 0) {
-        perror("head list lock init");
-        exit(-1);
-    }
+    // files to be zipped head
+    ll_node_t * file_list_head = NULL;
 
 
 
     if (file_count >= 1) { // One or more files are given
 
-        // Pass all given files to threads for processing
-        for (size_t idx = 0; idx < file_count; idx++) {
 
+        // List of pointers to list heads created for each file
+        if ((head_list = calloc(file_count, sizeof(ll_node_t *))) == NULL) {
+            perror("head_list");
+            exit(-1);
+        }
+        // Initialize lock file list heads
+        if (pthread_mutex_init(&head_list_lock, NULL) != 0) {
+            perror("head list lock init");
+            exit(-1);
+        }
+
+
+        // Initialize lock for busy thread count
+        if (pthread_mutex_init(&busy_th_count_lock, NULL) != 0) {
+            perror("stat list lock init");
+            exit(-1);
+        }
+
+
+        // add files to list
+        // every available thread is assigned next in this linked list
+        // to be processed and zipped
+        for (size_t idx = 0; idx < file_count; idx++) {
             th_data * data;
             if ((data = calloc(1, sizeof(th_data))) == NULL) {
-                perror("data alloc");
+                perror("add files to list");
                 exit(-1);
             }
             data->index = idx;
             data->filename = argv[idx +1];
 
-            pthread_t thread_id;
-            if (pthread_create(&thread_id, NULL, threadFunction, (void *) data) != 0) {
-                perror("thread create");
+            // Init if doesn't exist
+            if (file_list_head == NULL) {
+                file_list_head = initList(data);
+
+            } else {
+                addToEndOfList(file_list_head, data);
+            }
+        }
+        
+
+        // As long as there are un-processed files do the while loop
+        ll_node_t * next_file = popLinkedList(&file_list_head);
+        int busy_cnt;
+        struct timespec loop_sleep, rem_sleep;
+        loop_sleep.tv_sec  = 0;
+        loop_sleep.tv_nsec = 10;
+
+        while (next_file != NULL)
+        {
+            
+            pthread_mutex_lock(&busy_th_count_lock);
+            busy_cnt = busy_th_count;
+            pthread_mutex_unlock(&busy_th_count_lock);
+
+            // if there are non busy threads give them jobs
+            if (th_count - busy_cnt > 0) {
+
+                th_data * data = (th_data *) next_file->data;
+
+                pthread_t thread_id;
+                if (pthread_create(&thread_id, NULL, threadFunction, (void *) data) != 0) {
+                    perror("thread create");
+                    exit(-1);
+                }
+                next_file = popLinkedList(&file_list_head);
+            }
+            
+            if (nanosleep(&loop_sleep, &rem_sleep) < 0) {
+                perror("nanosleep failed");
                 exit(-1);
             }
-            thread_list[idx] = &thread_id;
-
-            //printf("Thread sent: %ld\n", idx);
         }
+        
+        
+        // crude but a working way to wait for all threads to finish
+        time_t start, now;
+        time(&start);
 
+        while (1)
+        {
+            pthread_mutex_lock(&busy_th_count_lock);
+            if (busy_th_count < 1) break;
+            pthread_mutex_unlock(&busy_th_count_lock);
 
-        // Wait for all threads to finish
-        for (size_t idx = 0; idx < file_count; idx++) {
-            pthread_t * thread = thread_list[idx];
-            pthread_join(*thread, NULL);
+            time(&now);
+            if (difftime(now, start) > 3) {
+                fprintf(stderr, "threads taking longer than 3 seconds");
+                break;
+            }
+
+            if (nanosleep(&loop_sleep, &rem_sleep) < 0) {
+                perror("nanosleep failed");
+                exit(-1);
+            }
         }
+        
 
 
         // Creating a chain of linked lists
         // 0 -> 1 -> 2 -> ...
         // get the first head as the main head
         main_head = head_list[0];
-        node_t * prev = main_head;
-        node_t * prev_tail = getTailLinkedList(prev);
-        node_t * curr;
+        if (main_head == NULL) {
+            fprintf(stderr, "list chain is null\n");
+            exit(-1);
+        }
+        ll_node_t * prev = main_head;
+        ll_node_t * prev_tail = getTailLinkedList(prev);
+        ll_node_t * curr;
 
         for (size_t idx = 1; idx < file_count; idx++) {
             // Get next in list as current
@@ -280,20 +402,26 @@ int main(int argc, char * argv[]) {
         curr = main_head;
         while (curr != NULL)
         {
-            * countP = curr->count;
-            * charP  = curr->character;
 
-            fwrite(countP, sizeof(int), 4, stdout);
-            fwrite(charP, sizeof(char), 1, stdout);
+            num_char_t * num_char_pair;
+            num_char_pair = (num_char_t *) curr->data;
+
+
+            * countP = num_char_pair->count;
+            * charP  = num_char_pair->character;
+
+            //fwrite(countP, sizeof(int), 4, stdout);
+            //fwrite(charP, sizeof(char), 1, stdout);
+            printf("%d%c ", *countP, *charP);
             curr = curr->next;
         }
 
         // free all allocated memory
         free(countP);
         free(charP);
-        free(thread_list);
         free(head_list);
 
+        freeLinkedList(file_list_head);
         freeLinkedList(main_head);
         pthread_mutex_destroy(&head_list_lock);
     }
